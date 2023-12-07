c077bd2490540e72f8497e6db206c7cb30d02164
Amit <patelamit812@gmail.com>
2023-12-07 10:29:26 +0530

    Microservices Communication | How USER SERVICE Communicate to RATING SERVICE

--- UserService/src/main/java/com/lcwd/user/service/services/impl/UserServiceImpl.java
+++ UserService/src/main/java/com/lcwd/user/service/services/impl/UserServiceImpl.java
@@ -1,12 +1,17 @@
 package com.lcwd.user.service.services.impl;
 
+import com.lcwd.user.service.entities.Rating;
 import com.lcwd.user.service.entities.User;
 import com.lcwd.user.service.exceptions.ResourceNotFoundException;
 import com.lcwd.user.service.repositories.UserRepository;
 import com.lcwd.user.service.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -15,6 +20,11 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private Logger logger = LoggerFactory.getLogger(UserService.class);
 
     @Override
     public User saveUser(User user) {
@@ -31,6 +41,14 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(String userId) {
-        return userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given id is not found on server !! : "+userId));
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given id is not found on server !! : " + userId));
+
+        //fetch rating of above user id
+        String ratingUrl = "http://localhost:8083/ratings/users/"+user.getUserId();
+        ArrayList<Rating> ratings = restTemplate.getForObject(ratingUrl, ArrayList.class);
+
+        user.setRatings(ratings);
+
+        return user;
     }
 }
